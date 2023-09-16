32e7a6b5625f212865cebe21dbd8705886e8ef5c
Efka97 <ejozokas@gmail.com>
2023-09-16 13:17:50 +0300

    Added react query to handle data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
-import { getWeather } from "./api/weatherApi";
 import WeatherPage from "./pages/WeatherPage/WeatherPage";
 
 function App() {
-  getWeather("Kaunas");
-
   return (
     <>
       <WeatherPage />
